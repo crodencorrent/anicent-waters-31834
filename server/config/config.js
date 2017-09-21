acf3906d9d8212dac80a9ccd02a311721c074f26
Cianan_Roden-Corrent <crodencorrent@ufl.edu>
2017-09-21 15:10:57 -0400

    added config

---
+++ server/config/config.js
@@ -0,0 +1,13 @@
+//This file holds any configuration variables we may need 
+//'config.js' is ignored by git to protect sensitive information, such as your database's username and password
+//copy this file's contents to another file 'config.js' and store your MongoLab uri there
+
+module.exports = {
+  db: {
+    uri: 'mongodb://test1:test2@ds127564.mlab.com:27564/cen3031_assign3', //place the URI of your mongo database here.
+  }, 
+  googleMaps: {
+    key: 'AIzaSyA7GWwUdUDTYQYi704o_j7TE2JqhkotwoI'
+  },
+  port: 8080
+};
