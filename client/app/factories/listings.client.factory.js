59f47b525b638e9145174a4b8e9c30d614308a44
Cianan_Roden-Corrent <crodencorrent@ufl.edu>
2017-09-21 16:19:55 -0400

    changed https stuff in factory

--- client/app/factories/listings.client.factory.js
+++ client/app/factories/listings.client.factory.js
@@ -2,23 +2,23 @@ angular.module('listings').factory('Listings', ['$http',
   function($http) {
     var methods = {
       getAll: function() {
-        return $http.get('http://localhost:8080/api/listings');
+        return $http.get('https://ancient-waters-41384.herokuapp.com/listings');
       },
 
       create: function(listing) {
-        return $http.post('http://localhost:8080/api/listings', listing);
+        return $http.post('https://ancient-waters-41384.herokuapp.com/listings', listing);
       }, 
 
       read: function(id) {
-        return $http.get('http://localhost:8080/api/listings/' + id);
+        return $http.get('https://ancient-waters-41384.herokuapp.com/listings/' + id);
       }, 
 
       update: function(id, listing) {
-        return $http.put('http://localhost:8080/api/listings/' + id, listing);
+        return $http.put('https://ancient-waters-41384.herokuapp.com/listings/' + id, listing);
       }, 
 
       delete: function(id) {
-        return $http.delete('http://localhost:8080/api/listings/' + id);
+        return $http.delete('https://ancient-waters-41384.herokuapp.com/listings/' + id);
       }
     };
 
